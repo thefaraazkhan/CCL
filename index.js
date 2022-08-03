7f8a47e2d97e42059034152e69950a6dc6cc12ad
thefaraazkhan <khanmohdfaraaz@gmail.com>
2022-08-04 00:00:25 +0530

    Added server logic (restful APIs)

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
+require("dotenv").config();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  // Serve static content
+  app.use(express.static(path.join(__dirname, "client/build")));
+  console.log(path.join(__dirname, "client/build"));
+}
+
+//Routes
+
+//create a todo
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+//get all todo
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodo = await pool.query("SELECT * FROM todo");
+    res.json(allTodo.rows);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+//get a todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+//update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    res.json("Todo was updated");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+//delete a todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json("Todo was deleted");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`Server runnin on http://localhost:${PORT}/`)
+);
