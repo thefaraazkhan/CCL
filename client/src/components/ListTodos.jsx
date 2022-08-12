b201883c4ebaf2124e2d7d2f9494b5f0030c96b3
thefaraazkhan <khanmohdfaraaz@gmail.com>
2022-08-12 07:05:38 +0530

    added new functions, fixed props for components

--- client/src/components/ListTodos.jsx
+++ client/src/components/ListTodos.jsx
@@ -1,18 +1,38 @@
 import { useEffect, useState } from 'react'
 import EditTodo from './EditTodo';
+import InputTodo from './InputTodo';
 
-function ListTodos() {
-
+const ListTodos = () => {
     const [todos, setTodos] = useState([]);
 
+
+    const addTodoHandler = (newTodo) => {
+        setTodos([...todos, newTodo]);
+    }
+
+    const updateDescriptionHandler = (todo_id, editedText) => {
+        getTodos();
+        let editedTodo = todos.map((todo) =>
+            todo.todo_id === todo_id ? { ...todo, description: editedText } : todo
+        )
+        setTodos(editedTodo);
+
+    };
+
     //Delete Todo function
-    const deleteTodo = async (id) => {
+    const deleteTodoHandler = async (todo_id) => {
         try {
-            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+            const deleteTodos = todos.filter((todo) => todo.todo_id === todo_id);
+
+            const updatedTodos = todos.filter((todo) => todo.todo_id !== todo_id);
+            setTodos(updatedTodos);
+
+            const backendUrl = process.env.REACT_APP_BACKEND_URL;
+            // console.log(`Backend URL DELETE: ${backendUrl}/todos/${id}`);
+            await fetch(`${backendUrl}/todos/${todo_id}`, {
                 method: "DELETE",
             });
-            setTodos(todos.filter(todo => todo.todo_id !== id));
-            console.log(deleteTodo);
+            console.log(`Deleted: ${deleteTodos[0].description}`);
         } catch (err) {
             console.error(err.message);
         }
@@ -20,9 +40,11 @@ function ListTodos() {
 
     const getTodos = async () => {
         try {
-            const response = await fetch("http://localhost:5000/todos");
+            const backendUrl = process.env.REACT_APP_BACKEND_URL;
+            const response = await fetch(`${backendUrl}/todos`);
             const jsonData = await response.json();
             setTodos(jsonData);
+            console.log(jsonData);
         } catch (err) {
             console.error(err.message);
         }
@@ -35,6 +57,9 @@ function ListTodos() {
     // console.log(todos);
     return (
         <div>
+            <h1 className="text-center mt-5">Todo List using PostgreSQL (PERN)</h1>
+            <InputTodo addTodoHandler={addTodoHandler} />
+
             <table className="table mt-5 text-center">
                 <thead>
                     <tr>
@@ -48,14 +73,22 @@ function ListTodos() {
                     {todos.map(todo => (
                         <tr key={todo.todo_id}>
                             <td>{todo.description}</td>
-                            <td><EditTodo todo={todo} /></td>
+                            <td>
+                                <EditTodo
+                                    todo_id={todo.todo_id}
+                                    description={todo.description}
+                                    updateDescriptionHandler={updateDescriptionHandler}
+                                    todos={todos}
+                                />
+                            </td>
                             <td>
                                 <button
-                                    className='btn btn-danger'
-                                    onClick={() => deleteTodo(todo.todo_id)}
+                                    className="btn btn-danger"
+                                    onClick={() => deleteTodoHandler(todo.todo_id)}
                                 >
                                     Delete
-                                </button></td>
+                                </button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
