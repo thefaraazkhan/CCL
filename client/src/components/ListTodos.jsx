a6d62d4bfd7ce5d15bcada598ba65578d1d2ca0b
thefaraazkhan <khanmohdfaraaz@gmail.com>
2022-08-13 10:10:39 +0530

    Added (set completed) feature in todo

--- client/src/components/ListTodos.jsx
+++ client/src/components/ListTodos.jsx
@@ -19,6 +19,25 @@ const ListTodos = () => {
 
     };
 
+    const toggleTodoHandler = async (todo_id) => {
+        try {
+            const updatedTodos = todos.map((todo) =>
+                todo.todo_id === todo_id ? { ...todo, completed: !todo.completed } : todo
+            );
+            setTodos(updatedTodos);
+
+            const updatedTodo = updatedTodos.find((t) => t.todo_id === todo_id);
+            const backendUrl = process.env.REACT_APP_BACKEND_URL;
+            await fetch(`${backendUrl}/todos/${todo_id}`, {
+                method: 'PATCH',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ completed: updatedTodo.completed }),
+            });
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
+
     //Delete Todo function
     const deleteTodoHandler = async (todo_id) => {
         try {
@@ -51,10 +70,19 @@ const ListTodos = () => {
     }
 
     useEffect(() => {
+        // Fetch and set initial todos from the backend
         getTodos();
-    }, [])
 
-    // console.log(todos);
+        // Fetch todos every 60 seconds
+        const fetchInterval = setInterval(() => {
+            getTodos();
+        }, 60000);
+
+        // Clean up the interval when the component is unmounted
+        return () => clearInterval(fetchInterval);
+    }, []);
+
+
     return (
         <div>
             <h1 className="text-center mt-5">Todo List using PostgreSQL (PERN)</h1>
@@ -64,15 +92,30 @@ const ListTodos = () => {
                 <thead>
                     <tr>
                         <th>Description</th>
+                        <th>Status</th>
                         <th>Edit</th>
                         <th>Delete</th>
                     </tr>
                 </thead>
-
                 <tbody>
                     {todos.map(todo => (
                         <tr key={todo.todo_id}>
-                            <td>{todo.description}</td>
+                            <td
+                                style={{
+                                    textDecoration: JSON.parse(todo.completed) ? 'line-through' : 'none',
+                                    color: JSON.parse(todo.completed) ? '#8C3333' : '#331D2C',
+                                }}
+                            >
+                                {todo.description}
+                            </td>
+                            <td>
+                                <button
+                                    className="btn btn-primary"
+                                    onClick={() => toggleTodoHandler(todo.todo_id)}
+                                >
+                                    {todo.completed ? "Mark Incomplete" : "Mark Complete"}
+                                </button>
+                            </td>
                             <td>
                                 <EditTodo
                                     todo_id={todo.todo_id}
@@ -92,10 +135,10 @@ const ListTodos = () => {
                         </tr>
                     ))}
                 </tbody>
-
             </table>
         </div>
-    )
+
+    );
 }
 
-export default ListTodos
+export default ListTodos;
